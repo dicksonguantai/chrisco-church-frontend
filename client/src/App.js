501642386fd08765494fa432e7b623b8df95f6a1
Davis <daviesnyakiba@gmail.com>
2024-02-16 13:27:36 +0300

    Update and style Footer component

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Footer />
     </div>
   );
 }
