16694251ed513c9470a08efac88f71b671ed0a41
Davis <daviesnyakiba@gmail.com>
2024-02-16 13:02:49 +0300

    Create the navbar for the various pages and the header outline

---
+++ client/src/components/Header.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+function Header(){
+    const churchTitle = "CENTRAL CHURCH - NAIROBI";
+    const logoUrl = "/logo.png"; 
+
+    return (
+        <header className="header">
+            <div className="left-container">
+                <img src={logoUrl} alt="Logo" className="logo" />
+                <span className="title">{churchTitle}</span>
+            </div>
+            <nav className="navbar">
+                <ul className="nav-links">
+                    <li className="nav-item">
+                        <Link to="/" >Home</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/about" >About Us</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/departments" >Departments</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/services" >Services</Link>
+                    </li>
+                </ul>
+            </nav>
+            <div>
+                <button className="button">Give</button>
+                <button className="button">Login</button>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
