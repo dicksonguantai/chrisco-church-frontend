2cc991fcae310c983b1eeecad62fd376973278f6
dicksonmutwiriguantai <dickson.mutwiri@student.moringaschool.com>
2024-02-19 20:09:16 +0300

    navigation to departments

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,26 @@
 import './App.css';
 import React from 'react';
 import Home from './components/Home';
-// import Departments from './components/Departments';
-
+// import About from './components/About';
+import Departments from './components/Departments';
+// import Services from './components/Services';
+// import Contact from './components/Contact';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* <Route path="/about" element={<About />} /> */}
+          <Route path="/departments" element={<Departments />} />
+          {/* <Route path="/services" element={<Services />} /> */}
+          {/* <Route path="/contact" element={<Contact />} /> */}
+          {/* Add more routes as needed */}
+        </Routes>
+      </div>
+    
   );
 }
 
