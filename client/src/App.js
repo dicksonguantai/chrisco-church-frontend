5f7be645a7ca28fd0fe0f47727f298d0c20bdb86
VERA-OBIERO <vera.obiero@gmail.com>
2024-02-20 13:49:05 +0300

    create Login and Signup components

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import React from 'react';
 import Home from './components/Home';
 // import About from './components/About';
 import Departments from './components/Departments';
+import Login from './components/Login';
+import Signup from './components/Signup';
 // import Services from './components/Services';
 // import Contact from './components/Contact';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
@@ -14,7 +16,9 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           {/* <Route path="/about" element={<About />} /> */}
-          <Route path="/departments" element={<Departments />} />
+           <Route path="/departments" element={<Departments />} />
+           <Route path="/login" element={<Login />} />
+           <Route path="/signup" element={<Signup />} />
           {/* <Route path="/services" element={<Services />} /> */}
           {/* <Route path="/contact" element={<Contact />} /> */}
           {/* Add more routes as needed */}
---
+++ client/src/components/Login.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './login.css';
+
+const LogIn = () => {
+  const handleForgotPassword = (e) => {
+    e.preventDefault();
+    console.log("Forgot password clicked");
+  };
+
+  return (
+    <div className="login-container">
+      <img src="./logo.svg" alt="Logo" className="login-logo" />
+      <h6>Don't have an account yet? <a href="/" className="signup-link">Sign up here</a></h6>
+      <form>
+        <div>
+          <input 
+            className="login-input"
+            type="email" 
+            placeholder="Email" 
+            required 
+          />
+        </div>
+        <div>
+          <input 
+            className="login-input"
+            type="password" 
+            placeholder="Password" 
+            required 
+          />
+          <div className="password-help">
+            <a href="/" onClick={handleForgotPassword}>Forgot your password?</a>
+          </div>
+        </div>
+        <button className="login-button" type="submit">Log In</button>
+      </form>
+    </div>
+  );
+};
+
+export default LogIn;
