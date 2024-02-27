961d523b14c52a10ef1273df0783025fd36040d1
dicksonmutwiriguantai <dickson.mutwiri@student.moringaschool.com>
2024-02-27 22:55:15 +0300

    services subheadings

--- client/src/components/Services.js
+++ client/src/components/Services.js
@@ -7,20 +7,20 @@ export default function Services() {
   return (
     <section className="service-sec">
       <div className="text-center mx-auto">
-        <h2 className="text-center text-2xl font-bold border-b border-black">
+        <h2 className="text-center text-2xl font-bold border-b border-black p-2">
           Service Program
         </h2>
         <div className="flex flex-wrap justify-center">
           <div className="service-item ">
-            <h4>Sunday Service</h4>
+            <h4 className="font-bold">Sunday Service</h4>
             <p>10:00 - 12:00 IN-PERSON & ONLINE SERVICE</p>
           </div>
           <div className="service-item ">
-            <h4>Mid week Service</h4>
+            <h4 className="font-bold">Mid week Service</h4>
             <p>5:30 - 7:00 PM IN-PERSON</p>
           </div>
           <div className="service-item ">
-            <h4>Overnight Service</h4>
+            <h4 className="font-bold">Overnight Service</h4>
             <p>10:00 - 6:00 PM IN-PERSON & ONLINE SERVICE</p>
           </div>
         </div>
